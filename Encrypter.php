92b82f0a1acca7bd7f1871826d9be800c1d51348
Himanshu <himanshu.phoolwar@otssolutions.com>
2018-03-08 14:04:09 +0530

    New libraries

---
+++ Encrypter.php
@@ -0,0 +1,129 @@
+<?php
+/**
+* Encrypter Class is a class which will encrypt data for the application
+*
+* Get the initial string combine it with a salt value 
+* (generated randomly through alphabets array) encoded 
+* it with base64 and then urlencoded then we add the 
+* alphabet in that string so that we can decrypt the 
+* string. Convert the alphabet into number place and 
+* you get the number of character used in the salt and 
+* you can remove that salt. 
+*
+* @category   houseViz
+* @package    Otssolutions
+* @version    1.0v
+* @since      06-10-2016
+* @Author     Himanshu Phoolwar
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Encrypter{
+    
+    private $eArray     = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    
+    private $len        = 0;
+    
+    private $randString = '';
+    
+    private $enString   = '';
+    
+    private $baseString = '';
+    
+    public function __constructor(){
+        
+    }
+    
+/*
+*   @function       : encryptionString
+*   @type           : public(access outside the class)
+*   @params         : string
+*   @return         : string
+*   @comments       : get the input and initailize all the methods for encryption
+*   @instruction    : $object->encryptionString($string);
+*/    
+    public function encryptionString($string = null){
+        if($string == null || !is_string($string)){
+            return 0;
+        }
+        $this->baseString = $string;
+        $this->getSaltLen();
+        $this->saltString();
+        $this->encryptedString();
+        return $this->enString;
+    }
+    
+
+/*
+*   @function       : getSaltLen
+*   @type           : private (access only inside the class)
+*   @params         : NULL
+*   @return         : NULL
+*   @comments       : Set len variable with alphabet with the help of eArray array.
+*   @instruction    : $this->getSaltLen();
+*/ 
+    private function getSaltLen(){
+        $this->len = array_rand($this->eArray);
+    }
+    
+/*
+*   @function       : saltString
+*   @type           : private (access only inside the class)
+*   @params         : NULL
+*   @return         : NULL
+*   @comments       : set randString variable using the len variable which will be used as salt string
+*   @instruction    : $this->getSaltLen();
+*/ 
+
+     private function saltString() {
+        
+        $char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($char);
+        for ($i = 0; $i < $this->len; $i++) {
+            $this->randString .= $char[rand(0, $charactersLength - 1)];
+        }
+    }
+    
+    
+/*
+*   @function       : encryptedString
+*   @type           : private (access only inside the class)
+*   @params         : NULL
+*   @return         : NULL
+*   @comments       : combine slat values and the accesskey value and also encode the string into byte data stream.
+*   @instruction    : $this->encryptedString();
+*/    
+    private function encryptedString(){
+        $key = $this->randString.$this->baseString;
+        $key = $this->bEncode($key);
+        $this->enString = $this->eArray[$this->len].$this->urlCoder($key);
+    }
+    
+     
+/*
+*   @function       : bEncode
+*   @type           : private (access only inside the class)
+*   @params         : (string)$string
+*   @return         : (string) base64 encoded
+*   @comments       : encode the given param into base64
+*   @instruction    : $this->bEncode($string);
+*/     
+    private function bEncode($string){
+        return base64_encode($string);
+    }
+    
+    
+/*
+*   @function       : urlCoder
+*   @type           : private (access only inside the class)
+*   @params         : (string)$string
+*   @return         : (string) urlencode 
+*   @comments       : encode the given param into urlencoded
+*   @instruction    : $this->urlCoder($string);
+*/    
+    private function urlCoder($string){
+        return urlencode($string);
+    }
+
+}
+
+?>
---
+++ Httpservice.php
@@ -0,0 +1,223 @@
+<?php
+/**
+* HttpService is a class use for sending and recieving web services details
+*
+*
+* @category   houseViz
+* @package    Otssolutions
+* @version    1.0v
+* @since      18-10-2016
+* @Author     Himanshu Phoolwar
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Httpservice{
+
+
+/*
+*   @function       : httpPost
+*   @type           : public(access outside the class)
+*   @params         : url, params, debug
+*   @return         : json object
+*   @comments       : curl function for web services propogation
+*   @instruction    : $object->httpPost($url,$params, $debug);
+*/  
+    public function httpPost($url,$params, $debug = null)
+    {
+        $params['source']   =   'houseviz';
+        $data_json = $this->jsonEncoder($params);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        if(!empty($debug)){
+            $info    = curl_getinfo($ch);
+            $this->pr($info);
+            $this->pr($response);
+            die('line number 40 at HttpService class from Houseviz ');
+        }
+        curl_close($ch);
+		$response = $this->jsonDecode($response);
+        return $response;
+
+    }
+    
+
+
+/*
+*   @function       : httpPut
+*   @type           : public(access outside the class)
+*   @params         : url, params, debug
+*   @return         : json object
+*   @comments       : curl function for web services propogation
+*   @instruction    : $object->httpPut($url,$params, $debug);
+*/ 
+    public function httpPut($url,$params, $debug = null)
+    {
+		$params['source']   =   'houseviz';
+        $data_json = $this->jsonEncoder($params);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        if(!empty($debug)){
+            $info    = curl_getinfo($ch);
+            $this->pr($info);
+            $this->pr($response);
+            die('line number 72 at HttpService class from Houseviz');
+        }
+        curl_close($ch);
+        $response = $this->jsonDecode($response);
+        return $response;
+    }
+	
+	/*
+	*   @function       : httpDelete
+	*   @type           : public(access outside the class)
+	*   @params         : url, params, debug
+	*   @return         : json object
+	*   @comments       : curl function for web services propogation
+	*   @instruction    : $object->httpPut($url,$params, $debug);
+	*/ 
+    public function httpDelete($url,$params,$debug = null)
+    {
+		$params['source']   =   'houseviz';
+        $data_json = $this->jsonEncoder($params);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        if(!empty($debug)){
+            $info    = curl_getinfo($ch);
+            $this->pr($info);
+            $this->pr($response);
+            die('line number 98 at HttpService class from Houseviz');
+        }
+        curl_close($ch);
+		$response = $this->jsonDecode($response);
+        return $response;
+    }
+	
+	
+	public function httpGet($url,$debug = null)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        if(!empty($debug)){
+            $info    = curl_getinfo($ch);
+            $this->pr($info);
+            $this->pr($response);
+            die('line number 117 at HttpService class from Houseviz');
+        }
+        curl_close($ch);
+		$response = $this->jsonDecode($response);
+        return $response;
+    }
+ 
+    
+/*
+*   @function       : httpAccepter
+*   @type           : public(access outside the class)
+*   @params         : null
+*   @return         : array
+*   @comments       : recieves the json data from the stream
+*   @instruction    : $object->httpAccepter();
+*/
+    public function httpAccepter()
+    {
+        $data = file_get_contents('php://input');
+        $result = $this->jsonDecode($data);
+		return $result;
+    }
+
+
+/*
+*   @function       : jsonEncoder
+*   @type           : private(access inside the class)
+*   @params         : string
+*   @return         : json string
+*   @comments       : convert string into json
+*   @instruction    : $object->jsonEncoder($string);
+*/
+    private function jsonEncoder($string)
+    {
+        return json_encode($string);
+    }
+    
+    
+/*
+*   @function       : jsonDecode
+*   @type           : private(access inside the class)
+*   @params         : string
+*   @return         : array
+*   @comments       : convert json to string
+*   @instruction    : $object->jsonDecode();
+*/
+    private function jsonDecode($string)
+    {
+        return json_decode($string, true);
+    }
+    
+    
+/*
+*   @function       : exitApp
+*   @type           : public(access inside the class)
+*   @params         : array
+*   @return         : null
+*   @comments       : convert result into json and terminate the connection
+*   @instruction    : $object->jsonDecode();
+*/
+    public function exitAppSuccess($data)
+    {
+        http_response_code(200);
+        echo $this->jsonEncoder($data);
+        exit();
+    }
+
+  
+/*
+*   @function       : pr
+*   @type           : private(access outside the class)
+*   @params         : content
+*   @return         : null
+*   @comments       : print data into the screen in a formated manner
+*   @instruction    : $object->pr();
+*/
+    public function pr($content)
+    {
+        echo '<pre>';
+        print_r($content);
+        echo '</pre>';
+    }
+	
+/*
+*   @function       : exitAppError
+*   @type           : public(access inside the class)
+*   @params         : int code,string errormsg
+*   @return         : null
+*   @comments       : convert result into json and terminate the connection
+*   @instruction    : $object->jsonDecode();
+*/	
+	
+	public function exitAppError($code,$errormsg)
+	{
+		http_response_code($code);
+		$ms=array("code"=>$code,"msg"=>$errormsg);
+        echo $this->jsonEncoder($ms);
+        exit();
+	}
+}
+
+?>
